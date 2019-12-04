ce71a10f4f2d5894ea316234a9d2cb609e89d408
Devanshu Jain <devanshu.jain@appirio.com>
2019-12-04 14:37:41 +0530

    Refactoring code according to form modal keys. Also, updated formJoson.json

--- src/Components/Dropdown/index.tsx
+++ src/Components/Dropdown/index.tsx
@@ -8,25 +8,24 @@ import {
   StyleSheet,
   Dimensions
 } from 'react-native';
-import { Card } from 'native-base';
-var { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 
-type formItems = {
-  id: number,
-  type: string;
-  label: string;
-  options: { id: number, value: string }[];
-  apiFormItem: string;
-  value: string,
+type formItem = {
+  label: string,
+  api_name: string,
+  input_type: string,
+  validation: { validation_msg_key: string, rule: string }[],
+  options: { value: string, label: string }[],
+  serviceCallName: null | string,
+  isSingleSelect: boolean,
+  dependentValue: null | string,
+  selectedValues: string
 };
 
 
 interface Props {
-  formItem: formItems;
-  // hideDropdownModal: Function;
-  // selectedItem: Function;
-  // visible: boolean
+  formItem: formItem;
 }
 
 interface State {
@@ -37,62 +36,63 @@ interface State {
 
 export default class Dropdown extends React.Component<Props, State> {
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      dropdownToRender: null,     // which dropdown modal to render
-      dropdownValue: null,        // selected dropdown value
-      modalVisible: false,        // to render dropdown modal or not
-    }
+  state = {
+    dropdownToRender: null,     // which dropdown modal to render
+    dropdownValue: null,        // selected dropdown value
+    modalVisible: false,        // to render dropdown modal or not
   }
 
   // Modal - dropdown options list
-  renderDropdownList(item: any) {
+  renderDropdownList(item: { value: string, label: string }) {
     return (
-      <TouchableOpacity style={styles.dropDownItem} onPress={() => this.selectItem(item.item.value)} >
-        <Text> {item.item.value} </Text>
+      <TouchableOpacity style={styles.dropDownItem} onPress={() => this.selectItem(item.label)} >
+        <Text> {item.label} </Text>
       </TouchableOpacity>
     )
   }
 
   // selected dropdown item
-  selectItem(value: any) {
+  selectItem(value: string) {
     this.setState({ modalVisible: false, dropdownValue: value })
   }
 
-  // show/hide dropdown modal and which dropdown modal to render
-  showDropdownModal(formItem: formItems) {
-    this.setState({ dropdownToRender: formItem.apiFormItem, modalVisible: !this.state.modalVisible })
+  // show/hide dropdown modal and setting which dropdown modal to render
+  showDropdownModal(formItem: formItem) {
+    this.setState({ dropdownToRender: formItem.api_name, modalVisible: !this.state.modalVisible })
+  }
+
+  // know whether to show/hide dropdown modal
+  visibleHideDropdownModal(formItem: formItem) {
+    return (this.state.dropdownToRender == formItem.api_name && this.state.modalVisible) ? true : false;
   }
 
   render() {
+    const { formItem } = this.props;
     return (
-      <View style={styles.formItemsView} key={this.props.formItem.id}>
-        <Text> {this.props.formItem.label} </Text>
-        <Card style={styles.textInputBorderView}>
+      <View style={styles.formItemsView} key={formItem.api_name}>
+        <Text> {formItem.label} </Text>
+        <View style={styles.textInputBorderView}>
           <Text style={styles.dropdownValue}>
             {this.state.dropdownValue}
           </Text>
-          <TouchableOpacity style={{ flex: 1 }}
-            onPress={() => this.showDropdownModal(this.props.formItem)}
-          />
-        </Card>
+          <TouchableOpacity style={{ flex: 1 }} onPress={() => this.showDropdownModal(formItem)} />
+        </View>
 
-        <Modal visible={(this.state.dropdownToRender == this.props.formItem.apiFormItem && this.state.modalVisible) ? true : false} animationType="slide">
+        <Modal visible={this.visibleHideDropdownModal(formItem)} animationType="slide">
           <TouchableOpacity
             onPress={() => this.setState({ modalVisible: !this.state.modalVisible })}
             style={styles.header}>
-            
-            <Text style={styles.headerLabel}> {this.props.formItem.label} </Text>
+            <View />
+            <Text style={styles.headerLabel}> {formItem.label} </Text>
             <Text style={styles.done}> Done </Text>
           </TouchableOpacity>
           <View style={{ flex: 1 }}>
             <FlatList
-              data={this.props.formItem.options}
+              data={formItem.options}
               renderItem={(item) => {
-                return this.renderDropdownList(item);
+                return this.renderDropdownList(item.item);
               }}
-              keyExtractor={item => JSON.stringify(item.id)}
+              keyExtractor={item => JSON.stringify(item.value)}
             />
           </View>
         </Modal>
@@ -106,12 +106,13 @@ export default class Dropdown extends React.Component<Props, State> {
 const styles = StyleSheet.create({
   formItemsView: { height: 100, justifyContent: 'center', paddingLeft: 10 },
   textInputBorderView: {
-    height: (55 / 667) * height,
+    height: (45 / 667) * height,
     width: (328 / 375) * width,
     borderRadius: 10,
     flexDirection: 'row',
     elevation: 1,
     borderColor: '#00000019',
+    borderWidth: 1
   },
   dropDownItem: {
     height: 50,
